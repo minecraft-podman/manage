142fa46a45609481a0045eed965499344d0c44f2
Jamie Bliss <jamie@ivyleav.es>
2020-01-03 01:11:43 -0500

    Remove debugging

--- manage/query/__init__.py
+++ manage/query/__init__.py
@@ -4,10 +4,10 @@ from starlette.routing import Route
 
 
 async def query_server(request):
-    with open('/etc/hosts') as f:
-        data = f.read()
-    return PlainTextResponse(data)
-    return JSONResponse({'hello': 'world'})
+    # with open('/etc/hosts') as f:
+    #     data = f.read()
+    # return PlainTextResponse(data)
+    return JSONResponse({'TODO': 'return server-list-ping data'})
 
 
 app = Starlette(debug=True, routes=[
